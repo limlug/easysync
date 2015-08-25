ef49d9c668e5a39e395b1d92fbcbd3789c6f7203
limlug <limlug@limlug.de>
2015-08-26 01:17:39 +0200

    Added Features

---
+++ index.php
@@ -0,0 +1,19 @@
+<html>
+<head>
+  <meta http-equiv="Content-Type" content="text/html;" />
+  <title>Easy Sync</title>
+</head>
+<body>
+  <div id="Container">
+    <div id="Header">
+    </div>
+    <div id="Navbar">
+      <a href="jobs.php">Jobs</a>
+      <a href="hosts.php">Hosts</a>
+      <a href="log.php">Log</a>
+    </div>
+    <div id="Content">
+    </div>
+  </div>
+</body>
+</html>
